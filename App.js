3a2839a097fb0bca051e39375808c597047afdda
nesrine345 <nesrine.bradai12@gmail.com>
2022-02-08 13:53:37 +0100

    new-try

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Cart from './component/Cart';
+import { Header } from './component/Header';
+import Home from './component/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+
+
+  );
+}
+
+export default App;
